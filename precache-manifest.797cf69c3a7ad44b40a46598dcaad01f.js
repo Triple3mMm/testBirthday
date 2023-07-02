27c81f51eb98051273fd8a1ef2d4cf3933b4c4f3
Triple3mMm <107885854+Triple3mMm@users.noreply.github.com>
2023-07-02 14:47:33 +0300

    Add files via upload

---
+++ precache-manifest.797cf69c3a7ad44b40a46598dcaad01f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1f7ebb3ee8f6f41480f484d96ea735",
+    "url": "/birthday-wisher/index.html"
+  },
+  {
+    "revision": "87e51253f0f0bc61e9a3",
+    "url": "/birthday-wisher/static/css/main.93a72abb.chunk.css"
+  },
+  {
+    "revision": "24af797f2d40df003816",
+    "url": "/birthday-wisher/static/js/2.34ffa3b0.chunk.js"
+  },
+  {
+    "revision": "9feb284d92501b45ec6e6f4f0fb649b3",
+    "url": "/birthday-wisher/static/js/2.34ffa3b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87e51253f0f0bc61e9a3",
+    "url": "/birthday-wisher/static/js/main.e53a124e.chunk.js"
+  },
+  {
+    "revision": "5b97457a1dabc9d16247",
+    "url": "/birthday-wisher/static/js/runtime-main.49da0469.js"
+  }
+]);
